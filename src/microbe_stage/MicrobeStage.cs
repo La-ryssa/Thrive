55f390e47ef30778e50c42bcea5565b4128de26d
adQuid <28126232+adQuid@users.noreply.github.com>
2025-02-22 12:24:57 -0500

    close panels the right way

--- src/microbe_stage/MicrobeStage.cs
+++ src/microbe_stage/MicrobeStage.cs
@@ -290,7 +290,7 @@ public partial class MicrobeStage : CreatureStageBase<Entity, MicrobeWorldSimula
                 {
                     TutorialState.SendEvent(TutorialEventType.MicrobeCompoundsNearPlayer,
                         new EntityPositionEventArgs(Clouds.FindCompoundNearPoint(playerPosition.Position,
-                            Compound.Glucose)),
+                            Compound.Glucose), Player.Get<CompoundStorage>().Compounds),
                         this);
                 }
 
@@ -306,7 +306,7 @@ public partial class MicrobeStage : CreatureStageBase<Entity, MicrobeWorldSimula
                         WorldSimulation);
 
                     TutorialState.SendEvent(TutorialEventType.MicrobeChunksNearPlayer,
-                        new EntityPositionEventArgs(position), this);
+                        new EntityPositionEventArgs(position, Player.Get<CompoundStorage>().Compounds), this);
                 }
 
                 guidancePosition = TutorialState.GetPlayerGuidancePosition();
